fdf5d5aeaf9f6abfcecee28fb0da7e29b76a3549
prerna-gupta02 <77267840+prerna-gupta02@users.noreply.github.com>
2021-06-06 12:43:38 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  var year = new Date().getFullYear();
+  return (
+    <div>
+      <footer>
+        <p>Copyright @ {year}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
